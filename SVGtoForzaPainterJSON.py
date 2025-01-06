38de3e6f33be617f2bd1cbfc0fb1c8a0282a9b75
endarz <thecaptainenderzxd@gmail.com>
2025-01-06 15:04:43 -0600

    First pass on horizontal optimization. 🥳

--- SVGtoForzaPainterJSON.py
+++ SVGtoForzaPainterJSON.py
@@ -3,7 +3,7 @@
 # via forza-painter.
 #
 # Created by endarz
-# Last edit: 1/4/2025
+# Last edit: 1/6/2025
 # Licenced under MIT License.
 
 import sys
@@ -13,15 +13,45 @@ import os.path
 
 # For a rect SVG string, returns its x-value.
 def getRectXValue(str):
-    return str[rect.find('"') + 1 : rect.find('" y="')] # Some unhinged, absolutely mental string hacking going on here!!!
+    # Some unhinged, absolutely mental string hacking going on here!!!
+    return int(str[str.find('"') + 1 : str.find('" y="')])
 
 # For a rect SVG string, returns its y-value.
 def getRectYValue(str):
-    return str[rect.find('y="') + 3 : rect.find('" w')]
+    return int(str[str.find('y="') + 3 : str.find('" w')])
+
+# For a rect SVG string, returns its width value.
+def getRectWidth(str):
+    return int(str[str.find('width="') + 7 : str.find('" h')])
+
+# For a rect SVG string, returns its height value.
+def getRectHeight(str):
+    return int(str[str.find('height="') + 8 : str.find('" f')])
 
 # For a rect SVG string, returns its fill color in hexadecimal.
 def getRectColor(str):
-    return str[rect.find('fill="') + 7 : rect.find('" /')]
+    return str[str.find('fill="#') + 7 : str.find('" /')]
+
+# For a rect SVG string, return a list representing a rect object.
+def getRectAsList(str):
+    rect_list = [getRectXValue(str),
+            getRectYValue(str),
+            getRectWidth(str),
+            getRectHeight(str),
+            getRectColor(str)]
+    return rect_list
+
+# For a rect as a list, convert it into a string of SVG.
+def svg(rect_list):
+    svg_str = '<rect ' + 'x="' + str(rect_list[0])
+    svg_str += '" y="' + str(rect_list[1])
+    svg_str += '" width="' + str(rect_list[2])
+    svg_str += '" height="' + str(rect_list[3])
+    svg_str += '" fill="#' + rect_list[4]
+    svg_str += '" />'
+
+    return svg_str
+
 
 # Main method.
 
@@ -56,12 +86,11 @@ if not file_path.endswith('.svg'):
 
 # Optimization phase.
 
-# Create a temporary file to perform destructive operations on.
-temp = open(os.path.basename(file_path) + '.tmp', 'w')
-
 # Open the SVG and obtain its lines.
 svg = open(file_path, 'r')
 svg_lines = svg.readlines()
+svg_lines = svg_lines.reverse() # Reverse the list so that the list can be used like a stack.
+optimized_lines = []            # Optimized lines get appended here.
 
 # Choose what method to use for optimization.
 # Set to 0 to combine by row (horizontal optimization).
@@ -71,15 +100,29 @@ method_of_opt = 0
 match method_of_opt:
     case 0:
         # Horizontal optimization.
-        # If two rects share both a y-value AND a fill color,
-        # combine them.
 
-        # Pop two lines from the list.
-        line_1 = svg_lines.pop(0)
-        line_2 = svg_lines.pop(0)
+            # Pop two SVG lines from the list.
+            line_1 = svg_lines.pop()    # "Primary line." line_2 merges with it if possible.
+            line_2 = svg_lines.pop()    # "Secondary line." A potentially redundant line.
 
-        # Obtain 
+            while not line_2 == '</svg>':
+                # Get the y-value and color for both lines.
+                line_1_y = getRectYValue(line_1)
+                line_2_y = getRectYValue(line_2)
+                line_1_color = getRectColor(line_1)
+                line_2_color = getRectColor(line_2)
 
+                # If two rects share both a y-value AND a fill color, combine them.
+                if line_1_y == line_2_y and line_1_color == line_2_color:
+                    line_1_values = getRectAsList(line_1)   # Get line_1's values.
+                    line_1_values[2] += 1                   # Increment width value by one.
+                    line_1 = svg(line_1_values)             # Convert the values to an SVG string.
+                    line_2 = svg_lines.pop()                # Get a new secondary line.
+                else:
+                    optimized_lines.append(line_1)          # Append line_1. It cannot merge with line_2.
+                    line_1 = line_2                         # line_2 becomes the "primary line"
+                    line_2 = svg_lines.pop()                # Get a new secondary line.
+        
     case 1:
         # Vertical pass.
         # If two rects share both an x-value AND a fill color,
@@ -110,11 +153,11 @@ result.write(header)
 
 # Parse each rect in the SVG file. Turn it into a JSON line.
 i = 1
-for rect in svg_lines:
+for line in svg_lines:
     # Get the x, y and hexadecimal color code from the rect.
-    x = getRectXValue(rect)
-    y = getRectYValue(rect)
-    hex_color = getRectColor(rect)
+    x = getRectXValue(line)
+    y = getRectYValue(line)
+    hex_color = getRectColor(line)
 
     # Adjust the x and y values so the squares are seamless at 0.01 scale.
     # Put the values in their respective places in the data list.
