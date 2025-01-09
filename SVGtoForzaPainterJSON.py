1d733ada5df44c585a6fc28b256a3537fe8d4754
endarz <thecaptainenderzxd@gmail.com>
2025-01-08 19:39:46 -0600

    Main optimization pass complete. Bugs to be fixed, though.

--- SVGtoForzaPainterJSON.py
+++ SVGtoForzaPainterJSON.py
@@ -158,132 +158,42 @@ match method_of_opt:
         # and replace the secondary pixel with None in the pixel grid.
         print('Starting horizontal merging...')
         for row in pixel_grid:
-            print('Starting new row.')
             for i in range(len(row)):
-                print(i)
                 pri_pix = row[i]
                 sec_pix = row[i + 1]
-                print(pri_pix)
-                print(sec_pix)
                 if pri_pix == None or sec_pix == None:  # If either of the pixels is a None...
                     if i == len(row) - 2:   # Edge case: if the last pixel in the row is a none, leave this row.
-                        print('Last pixel in the row is None. Leaving this row...')
                         break
                     else:                   # Otherwise, go to the next iteration.
-                        print('One of the pixels is None. Reiterating...')
                         continue
                 # Check if the secondary pixel's color matches primary pixel's color.
                 # If it does, then merge.
-                if sec_pix[4] == pri_pix[4]:
-                    print('Color match found, will merge.')
-                else:
-                    print('Color does not match, will not merge.')
                 while sec_pix[4] == pri_pix[4]:
-                    sys.stdout.write('Merging ')
-                    sys.stdout.write(str(pri_pix))
-                    sys.stdout.write(' with ')
-                    sys.stdout.write(str(sec_pix))
-                    print('')
-
                     pri_pix[2] += 1                                 # Increment primary pixel's width by one.
                     pixel_grid[pri_pix[1]][pri_pix[0]] = pri_pix    # Update primary pixel in the pixel grid.
                     pixel_grid[sec_pix[1]][sec_pix[0]] = None       # Update secondary pixel in the pixel grid.
                     row[sec_pix[0]] = None                          # Update secondary pixel in the row.
 
-                    print('Row:')
-                    print(row)
                     # Check if the last pixel in the row just got merged.
                     # If it did, then we need to leave and start a new row.
                     if sec_pix[0] == len(row) - 1:
-                        print('Last pixel in the row just got merged.')
                         break
                     else:
-                        print('Getting next secondary pixel...')
                         sec_pix = row[sec_pix[0] + 1]               # Get the pixel following the secondary pixel.
                         if sec_pix == None:                         # If the new secondary pixel is a None, exit the loop.
                             break
                 if sec_pix == None or sec_pix[0] == len(row) - 1:
                     break
-                    
         print('Horizontal merging complete.')
-        print('Pixel grid:')
-        for row in pixel_grid:
-            print(row)
     case 1:
         pass
-
-exit("Script completed!")
-
-
-
-
-"""
-optimized_lines = []    # Optimized lines get appended here.
-
-# Choose what method to use for optimization.
-# Set to 0 to combine by row (horizontal optimization).
-# Set to 1 to combine by column (vertical optimization).
-method_of_opt = 0
-match method_of_opt:
-    case 0:
-        # Horizontal optimization.
-
-            # Pop two SVG lines from the list.
-            line_1 = svg_lines.pop()    # String that is the "primary line." line_2 merges with it if possible.
-            line_2 = svg_lines.pop()    # String that is the "secondary line," a potentially redundant line.
-
-            while not line_2 == '</svg>':
-                # Get x values.
-                line_1_x = getRectXValue(line_1)
-                line_2_x = getRectXValue(line_2)
-                # Get y values.
-                line_1_y = getRectYValue(line_1)
-                line_2_y = getRectYValue(line_2)
-                # Get colors.
-                line_1_color = getRectColor(line_1)
-                line_2_color = getRectColor(line_2)
-
-                count = 0
-                # If the two pixels share a y-value and color, prepare to look ahead.
-                if line_1_y == line_2_y and line_1_color == line_2_color:
-                    count += 1
-                    prev_x = line_2_x
-                    line_2 = svg_lines.pop()
-
-
-                    line_1_values = getRectAsList(line_1)   # Get line_1's values.
-                    line_1_values[2] +=                # Increment width value by count.
-                    # Create a new SVG string from the new values.
-                    # I tried to make this its own method, but Python didn't like it.
-                    chunks = ['<rect ',
-                                'x="',
-                                line_1_values[0].__str__(),
-                                '" y="',
-                                line_1_values[1].__str__(),
-                                '" width="',
-                                line_1_values[2].__str__(),
-                                '" height="',
-                                line_1_values[3].__str__(),
-                                '" fill="#',
-                                line_1_values[4],
-                                '" />\n']           
-                    line_1 = ''.join(chunks)
-                    line_2 = svg_lines.pop()                # Get a new secondary line.
-
-                else:
-                    optimized_lines.append(line_1)          # Append line_1. It cannot merge with line_2.
-                    line_1 = line_2                         # line_2 becomes the "primary line"
-                    line_2 = svg_lines.pop()                # Get a new secondary line.
-    case 1:
-        # Vertical pass. Has not been implemented yet.
-        # If two rects share both an x-value AND a fill color,
-        # combine them.
-        pass
+print('Merging complete.')
 
 #
-# Conversion to JSON Phase
+#   JSON Generation Phase
 #
 
+print('Starting JSON file generation...')
 # Create the result JSON file.
 result = open(os.path.basename(file_path) + '.json', 'w+')
 
@@ -294,50 +204,58 @@ type = 1048677                  # The "shape ID." This value is a square.
 data = [0, 0, 0, 0, 0, 0, 0]    # X position; Y position; scale X; scale Y; rotation; and two other values that I don't know.
 color = [255, 255, 255, 255]    # Red, Green, Blue and Alpha values
 score = 0.8008135               # Shape accuracy; used by forza-painter. :]
+json_shapes = []                # Stores the shapes after they are converted to JSON.
 
 # Begin the parsing process.
 result.write(header)
 
-# Parse each rect in the SVG file. Turn it into a JSON line.
-i = 1
-for line in optimized_lines:
-    # Get the x, y and hexadecimal color code from the rect.
-    x = getRectXValue(line)
-    y = getRectYValue(line)
-    hex_color = getRectColor(line)
+# Parse each pixel in the pixel grid, turning it into JSON.
+for row in pixel_grid:
+    for pixel in row:
+        # Skip to next pixel when a None is found.
+        if pixel == None:
+            continue
+        else:
+            # Get the x, y and hexadecimal color code from the rect.
+            x = pixel[0]
+            y = pixel[1]
+            hex_color = pixel[4]
 
-    # Adjust the x and y values so the rectangles are seamless at 0.01 scale.
-    # Put the values in their respective places in the data list.
-    x = (float(x) * 1.28) + ((getRectWidth(line) * 1.28) / 2)
-    data[0] = x
-    y = (float(y) * 1.28) + ((getRectHeight(line) * 1.28) / 2)
-    data[1] = y
+            # Adjust the x and y values so the rectangles are seamless at 0.01 scale.
+            # Put the values in their respective places in the data list.
+            x = (float(x) * 1.28) + ((pixel[2] * 1.28) / 2)
+            data[0] = x
+            y = (float(y) * 1.28) + ((pixel[3] * 1.28) / 2)
+            data[1] = y
 
-    # Set width and height of rectangle.
-    data[2] = 0.01 * getRectWidth(line)
-    data[3] = 0.01 * getRectHeight(line)
+            # Set width and height of rectangle.
+            data[2] = 0.01 * pixel[2]
+            data[3] = 0.01 * pixel[3]
 
-    # Convert hex color to RGB values.
-    color[0] = int(hex_color[0:2], 16)
-    color[1] = int(hex_color[2:4], 16)
-    color[2] = int(hex_color[4:6], 16)
+            # Convert hex color to RGB values.
+            color[0] = int(hex_color[0:2], 16)
+            color[1] = int(hex_color[2:4], 16)
+            color[2] = int(hex_color[4:6], 16)
 
-    # Write the rectangle into the JSON
-    result.write('{')
-    result.write('"type":' + str(type) + ',')
-    result.write('"data":' + str(data).replace(' ', '') + ',')
-    result.write('"color":' + str(color).replace(' ', '') + ',')
-    result.write('"score":' + str(score))
-    result.write('}')
-    # Decide if the end of the JSON has been reached.
-    if i < len(optimized_lines):
-        result.write(',\n')
-    i += 1
+            # Create a string of JSON.
+            json = '{'
+            json += '"type":' + str(type) + ','
+            json += '"data":' + str(data).replace(' ', '') + ','
+            json += '"color":' + str(color).replace(' ', '') + ','
+            json += '"score":' + str(score)
+            json += '}'
 
+            # Add to JSON shapes list.
+            json_shapes.append(json)
+
+# Write json shapes to the file.
+result.write(',\n'.join(json_shapes))
 
 # End the parsing process.
 result.write(trailer)
 result.close()
+print('JSON file created at ' + os.getcwd() + os.path.basename(file_path) + '.json.')
 
 print('Done! The result file has been placed in this script\'s directory.')
-"""
+
+exit(0)
